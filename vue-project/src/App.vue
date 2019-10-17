11a00431d927d3e6ddd7b4f0898809281c785b03
Jin-Jane <j.songprakob@gmai.com>
2019-10-17 12:14:06 +0700

    use coreUI template

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,23 +1,41 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+    <AppHeader fixed>
+      <SidebarToggler class="d-lg-none" display="md" mobile />
+      <b-link class="navbar-brand" to="#">
+        <!-- <img class="navbar-brand-full" src="img/brand/logo.svg" width="89" height="25" alt="CoreUI Logo">
+        <img class="navbar-brand-minimized" src="img/brand/sygnet.svg" width="30" height="30" alt="CoreUI Logo"> -->
+      </b-link>
+      <SidebarToggler class="d-md-down-none" display="lg" :defaultOpen=true />
+      <b-navbar-nav class="d-md-down-none">
+        <b-nav-item class="px-3" to="/dashboard">Dashboard</b-nav-item>
+        <b-nav-item class="px-3" to="/users" exact>Users</b-nav-item>
+        <b-nav-item class="px-3">Settings</b-nav-item>
+      </b-navbar-nav>
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item class="d-md-down-none">
+          <i class="icon-bell"></i>
+          <b-badge pill variant="danger">5</b-badge>
+        </b-nav-item>
+        <b-nav-item class="d-md-down-none">
+          <i class="icon-list"></i>
+        </b-nav-item>
+        <b-nav-item class="d-md-down-none">
+          <i class="icon-location-pin"></i>
+        </b-nav-item>
+      </b-navbar-nav>
+      <AsideToggler class="d-none d-lg-block" />
+      <!--<AsideToggler class="d-lg-none" mobile />-->
+    </AppHeader>
 </template>
-
 <script>
+import { Header as AppHeader, SidebarToggler, AsideToggler } from '@coreui/vue'
+
 export default {
-  name: 'App'
+  name: 'DefaultHeader',
+  components: {
+    AsideToggler,
+    AppHeader,
+    SidebarToggler
+  }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
